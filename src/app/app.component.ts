5f9fc04052c8f9150f5fa6fb1bcd391a6e9d8abd
tim <tim_chou@chowsangsang.com>
2020-05-27 09:59:05 +0800

    add input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Stock } from './model/stock';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'angular-ch6';
+export class AppComponent implements OnInit {
+  title = 'Stock Market App';
+
+  public stock: Stock;
+
+  ngOnInit(): void {
+    this.stock = new Stock('Test Stock Company', 'TSC', 85, 80);
+  }
+
+  onToggleFavorite(stock: Stock) {
+    console.log('Favoriate for stock', stock, ' was troggered');
+    this.stock.favorite = !this.stock.favorite;
+  }
 }
